3dc6b2fc5b6dd96866ba4897b145d5372a72d360
NikitaRom666 <romanenko.n_ipz23@rcit.ukr.educatioon>
2025-09-26 15:16:54 +0300

    lab1: fix Figure.cs (save class implementation)

---
+++ lab1/Figure.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace lab1
+{
+    public class Figure
+    {
+        private double area;
+
+        public Figure(double area)
+        {
+            this.area = area >= 0 ? area : 0;
+            Console.WriteLine($"[Figure] Конструктор: Area = {this.area}");
+        }
+
+        ~Figure()
+        {
+            // Destructor
+        }
+
+        public double Area
+        {
+            get { return area; }
+            set { area = value >= 0 ? value : 0; }
+        }
+
+        public string GetFigure()
+        {
+            return $"Figure {{ Area = {area} }}";
+        }
+    }
+}
