d5ec900c06dcb085343dc6533fe0b68a046f5fdd
NikitaRom666 <romanenko.n_ipz23@rcit.ukr.educatioon>
2025-09-26 15:26:33 +0300

    Повністю оновлено lab1: Figure, Program і проєктні файли

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
+<<<<<<< HEAD
 # OOP-Romanenko
+=======
+﻿# OOP-Romanenko
+
+Lab1: клас Figure з властивістю Area, конструктором, деструктором і методом GetFigure().
+>>>>>>> eafe7be (lab1: add Figure, Program and project files)
---
+++ lab1/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace lab1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Figure fig = new Figure(42.5);
+            Console.WriteLine("Вивід: властивість Area та GetFigure():");
+            Console.WriteLine($"Area property: {fig.Area}");
+            Console.WriteLine(fig.GetFigure());
+            Console.WriteLine("Натисніть будь-яку клавішу для виходу...");
+            Console.ReadKey();
+        }
+    }
+}
