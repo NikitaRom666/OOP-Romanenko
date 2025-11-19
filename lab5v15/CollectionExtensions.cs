495fdad95ac149929f281d85a82114565c4c7d06
NikitaRom666 <romanenko.n_ipz23@rcit.ukr.educatioon>
2025-11-19 11:45:11 +0200

    Lab5: Composition + Generics + Exceptions

---
+++ lab5v15/CollectionExtensions.cs
@@ -0,0 +1,24 @@
+// CollectionExtensions.cs
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+public static class CollectionExtensions
+{
+    // T TopN<T>(IEnumerable<T>, int, Func<T,double>)
+    // Узагальнений метод для вибору N елементів, що мають найбільше значення за заданим критерієм.
+    public static IEnumerable<T> TopNByValue<T>(
+        this IEnumerable<T> source, 
+        int count, 
+        Func<T, double> valueSelector)
+    {
+        if (source == null) throw new ArgumentNullException(nameof(source));
+        if (count <= 0) return Enumerable.Empty<T>();
+        
+        // Сортуємо колекцію за значенням критерію у порядку спадання (OrderByDescending)
+        // та беремо перші 'count' елементів (Take).
+        return source
+            .OrderByDescending(valueSelector)
+            .Take(count);
+    }
+}
