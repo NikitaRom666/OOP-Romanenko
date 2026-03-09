b27ba2809e725489e76d6602c2daf5873e3600f4
NikitaRom666 <romanenko.n_ipz23@rcit.ukr.education>
2026-03-09 15:16:53 +0200

    Lab30 Variant13: CurrencyConverter with xUnit tests

---
+++ lab30v13/CurrencyConverter.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace lab30v13
+{
+    public class CurrencyConverter
+    {
+        private readonly Dictionary<string, decimal> rates = new()
+        {
+            {"USD", 1m},
+            {"EUR", 0.93m},
+            {"UAH", 36.9m},
+            {"GBP", 0.81m}
+        };
+
+        public decimal GetRate(string currency)
+        {
+            if (!rates.ContainsKey(currency))
+                throw new ArgumentException("Currency not supported");
+
+            return rates[currency];
+        }
+
+        public decimal Convert(decimal amount, string fromCurrency, string toCurrency)
+        {
+            if(amount < 0) throw new ArgumentException("Amount cannot be negative");
+            if (!rates.ContainsKey(fromCurrency) || !rates.ContainsKey(toCurrency))
+                throw new ArgumentException("Currency not supported");
+
+            decimal usdAmount = amount / rates[fromCurrency]; // конвертуємо у USD
+            return usdAmount * rates[toCurrency];
+        }
+    }
+}
